cee36641032dfcf0d4ac4404d0e98c4ba75b3f21
satorimon <48250071+satorimon@users.noreply.github.com>
2019-07-08 21:43:52 +0900

    初回

---
+++ ServiceContract/Interface1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorlddServiceContract
+{
+    [ServiceContract(Namespace = "http://HelloWorlddServiceContract")]
+    public interface IHelloWorldService
+    {
+        [OperationContract]
+        string SayHello(string name);
+    }
+
+}
